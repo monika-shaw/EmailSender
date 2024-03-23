34f8e51b774f7ac31c76b6316ce717e645fce924
shawmonika616ms <shawmonika616ms@gmail.com>
2024-03-23 20:40:39 +0530

    Description : logic is completed.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express')
 var http = require('http')
 var path = require('path')
-//var modeMailer = require('nodemailer')
+var nodemailer = require('nodemailer')
 
 var app = express();
-var server = http.Server(app)
-var port = 500;
+var server = http.createServer(app)
+var port = 5000;
 
 app.set("port", port)
-app.use(express.json)
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, "page/index.html")))
 
@@ -19,6 +19,35 @@ app.get("/", function(req, res){
 })
 
 
+app.post('/send_email', function(req, res){
+    var from = req.body.from
+    var to = req.body.to
+    var subject = req.body.subject
+    var message = req.body.message
+
+    var trnasporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:'shawmonika616ms@gmail.com',
+            pass:'kgnk bkhk dvzk ruum'
+        }
+    })
+    var mailOptions={
+        from:from,
+        to:to,
+        subject:subject,
+        text:message
+    }
+
+    trnasporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Email Sent", +info.res);
+        }
+        res.redirect("/")
+    })
+})
 //initialize web server
 
 server.listen(port, function(){
